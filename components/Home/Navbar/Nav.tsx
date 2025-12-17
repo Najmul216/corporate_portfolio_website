c4f82931fd4e790f39c3fd9588c00793ee428cf9
najmulHasanChowdhury <hnajmul39@gmail.com>
2025-12-17 09:00:46 +0600

    Add NavLinks

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,6 +1,7 @@
 import Logo from "@/components/Helper/Logo";
+import { NavLINKS } from "@/constant/constant";
+import Link from "next/link";
 import React from "react";
-import { BiSolidBusiness } from "react-icons/bi";
 
 const Nav = () => {
   return (
@@ -8,6 +9,19 @@ const Nav = () => {
       <div className="flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto">
         {/* Logo Section */}
         <Logo />
+        {/* Nav links for large screens */}
+        <div className="hidden lg:flex bg-white rounded-full h-[10vh] md:p-4 items-center space-x-10">
+          {NavLINKS.map((link) => (
+            <Link
+              key={link.id}
+              href={link.url}
+              className="text-black hover:text-green-500 font-semibold transition-all duration-300"
+            >
+              <p>{link.lable}</p>
+            </Link>
+          ))}
+          {/* <button>Create Account</button> */}
+        </div>
       </div>
     </div>
   );
---
+++ constant/constant.ts
@@ -0,0 +1,39 @@
+import { url } from "inspector";
+
+export const NavLINKS = [
+    {
+        id: 1,
+        url: "#",
+        lable: "Home",
+    },
+    {
+        id: 2,
+        url: "#",
+        lable: "About Us",
+    },
+    {
+        id: 3,
+        url: "#",
+        lable: "Services",
+    },
+    {
+        id: 4,
+        url: "#",
+        lable: "Work",
+    },
+    {
+        id: 5,
+        url: "#",
+        lable: "Portfolio",
+    },
+    {
+        id: 6,
+        url: "#",
+        lable: "Blog",
+    },
+    {
+        id: 7,
+        url: "#",
+        lable: "Contact",
+    },
+]

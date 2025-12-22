4c3e63de9c73135c4eff3af983848697d2ba32b3
najmulHasanChowdhury <hnajmul39@gmail.com>
2025-12-22 11:10:14 +0600

    Add about page

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./Hero/Hero";
+import AboutPage from "./AboutPage/AboutPage";
 
 const Home = () => {
   return (
     <div className="overflow-hidden text-white">
       <Hero />
+      <AboutPage />
     </div>
   );
 };

28223c3f5709eeba6b3814ad6b0e68818908c426
najmulHasanChowdhury <hnajmul39@gmail.com>
2025-12-24 21:21:32 +0600

    Add Services constants

--- constant/constant.ts
+++ constant/constant.ts
@@ -27,13 +27,47 @@ export const NavLINKS = [
         lable: "Portfolio",
     },
     {
-        id: 6,
-        url: "#",
-        lable: "Blog",
-    },
-    {
         id: 7,
         url: "#",
-        lable: "Contact Us",
+        lable: "Career",
+    },
+]
+
+export const SERVICES = [
+    {
+        id: 1,
+        icon: "🏢",
+        title: "Engineering Projects Management",
+        description: "We develop and understand project expectations and then manage those needs with a design team. Innovation should happen throughout a project.",
+    },
+    {
+        id: 2,
+        icon: "🧑",
+        title: "Man-Power Operations and Maintenance",
+        description: "We work closely with architects and designers to understand the project, and ultimately develop a targeted solution that optimizes every stage of the build.",
+    },
+    {
+        id: 3,
+        icon: "💧",
+        title: "Water & Sewage Pump Stations",
+        description: "EPCC Services provides the right resources and expertise to evaluate concepts in building high standard Water & Sewage Pump Stations.",
+    },
+    {
+        id: 4,
+        icon: "🏗️",
+        title: "Construction Services",
+        description: "EPCC provides the right resources and expertise to evaluate concepts through our industry leading Preconstruction Services team.",
+    },
+    {
+        id: 5,
+        icon: "🏛️",
+        title: "Civil Departments and Buildings",
+        description: "In the past decade alone, we have completed more than 5 million square feet of construction building.",
+    },
+    {
+        id: 6,
+        icon: "🚚",
+        title: "Industrial Works",
+        description: "We employs a talented team of industry leading professionals capable of self-performing complex concrete work, considered on a project specific basis.",
     },
 ]

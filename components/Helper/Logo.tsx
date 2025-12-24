5e3c4b27de2052afd7d6cb49508dbfda9cc9fef3
najmulHasanChowdhury <hnajmul39@gmail.com>
2025-12-24 21:21:46 +0600

    Update

--- components/Helper/Logo.tsx
+++ components/Helper/Logo.tsx
@@ -4,10 +4,10 @@ import { BiSolidBusiness } from "react-icons/bi";
 const Logo = () => {
   return (
     <div className="flex items-center space-x-2">
-      <div className="w-10 h-10 bg-green-400 rounded-full flex items-center justify-center flex-col">
+      <div className="w-14 h-14 bg-green-400 rounded-full flex items-center justify-center flex-col">
         <BiSolidBusiness className="w-6 h-6 text-white" />
       </div>
-      <h1 className="text-xl md:text-xl hidden sm:block text-white font-bold">
+      <h1 className="text-xl md:text-xl hidden sm:block text-black font-bold">
         Fortex Contracting Company Ltd.
       </h1>
     </div>

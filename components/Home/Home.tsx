2465291e776c8aab1fa47e7166c23eecc69e5d6e
najmulHasanChowdhury <hnajmul39@gmail.com>
2025-12-24 11:03:29 +0600

    Add Services components

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./Hero/Hero";
 import AboutPage from "./AboutPage/AboutPage";
+import Services from "./Services/Services";
 
 const Home = () => {
   return (
     <div className="overflow-hidden text-white">
       <Hero />
       <AboutPage />
+      <Services />
     </div>
   );
 };
